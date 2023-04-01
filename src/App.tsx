5c0f42772fa41e227e76a6102da2d656ba7ed165
marklin2289 <marklin2289@gmail.com>
2023-03-31 17:40:16 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+// import { useState } from "react";
+
+import { useEffect, useState } from "react";
+import { SelectedPage } from "./shared/types";
+import Navbar from "./scenes/navbar";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  // when scroll, color changes on Navbar items
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  );
+}
+
+export default App;
