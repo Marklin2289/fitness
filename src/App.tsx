4bd112a740571e89609438b31b23e141eb370f0c
marklin2289 <marklin2289@gmail.com>
2023-03-31 18:27:37 -0700

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 import Navbar from "./scenes/navbar";
+import Home from "./scenes/home";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -31,6 +32,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
